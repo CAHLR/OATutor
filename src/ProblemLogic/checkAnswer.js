09c8a3a17299f470dfa4da56829586cea3e07b7b
matthew29tang <matthew29tang@gmail.com>
2020-10-10 17:18:38 -0700

    Integrate Khan KAS

--- src/ProblemLogic/checkAnswer.js
+++ src/ProblemLogic/checkAnswer.js
@@ -1,8 +1,16 @@
 var Algebrite = require('algebrite');
+var KAS = require('../kas.js');
 
 // attempt = student answer, actual = [ans1, ans2]
 function _equality(attempt, actual) {
   return actual.some((stepAns) => (attempt === stepAns));
+}
+
+// attempt = student answer, actual = [ans1, ans2]
+function _parseEquality(attempt, actual) {
+  console.log("PARSED: " + attempt.print());
+  console.log("ANSWER: " + actual[0].print());
+  return actual.some((stepAns) => KAS.compare(attempt, stepAns).equal);
 }
 
 // Round to precision number of decimal places
@@ -14,15 +22,6 @@ function round(num, precision) {
   }
 }
 
-function _parseArithmetic(attempt, isAnswer) {
-  var parsed = attempt.replace(/\)\(/g, ')*(');
-  parsed = Algebrite.run(parsed).toString();
-  if (!isAnswer && parsed.includes("syntax error")) {
-    return "algebrite syntax error"
-  }
-  return parsed;
-}
-
 function checkAnswer(attempt, actual, answerType, precision) {
   var parsed = attempt;
   var correctAnswer = false;
@@ -30,8 +29,8 @@ function checkAnswer(attempt, actual, answerType, precision) {
     if (parsed === "") {
       return [parsed, false];
     } else if (answerType === "arithmetic") {
-      parsed = _parseArithmetic(attempt, false);
-      correctAnswer = _equality(parsed, actual.map((actualAns) => _parseArithmetic(actualAns, true)));
+      parsed = KAS.parse(attempt).expr;
+      correctAnswer = _parseEquality(parsed, actual.map((actualAns) => KAS.parse(actualAns).expr));
     } else if (answerType === "string") {
       parsed = attempt;
       correctAnswer = _equality(parsed, actual);
