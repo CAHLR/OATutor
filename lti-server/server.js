eb16c821cfc654c014c8e5f615e590005477aafc
matthew29tang <matthew29tang@gmail.com>
2020-11-12 22:42:45 -0800

    Update LTI server

--- lti-server/server.js
+++ lti-server/server.js
@@ -1,28 +1,74 @@
-var port = process.env.PORT || 8888; //sets local server port to 8888
+var port = process.env.PORT || 1339; //sets local server port to 1339
 var express = require('express'); // Express web server framework
+var https = require("https");
 var request = require('request');
+var bodyParser = require('body-parser')
+var lti = require('ims-lti');
 
-
-var redirect_uri = 'http://localhost:8888'; // Your redirect uri in case you are using apis
+var providers = {};
+var provider;
+var secret = 'openits-secret';
 var app = express();
 
-console.log("Starting up server... Making requests");
+app.use(bodyParser.json());                            // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));    // to support URL-encoded bodies
+console.log("Starting up server on port: " + port);
 
-var https = require("https");
-
-app.get('/auth', function(req, res) {
-  console.log("Displaying get");
+// Routes
+app.get('/auth', function (req, res) {
+  console.log("Auth get");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.send("Hello World");
+  res.send("Server only accepts post requests.");
 });
 
-app.post('/auth', function(req, res) {
-    console.log("Displaying post");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.send("Hello World"+req);
+app.post('/auth', function (req, res) {
+  /*
+  Takes in an LTI post request
+  */
+  console.log("Auth post");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  console.log(req.body);
+  provider = new lti.Provider('openits-key', secret);
+  provider.valid_request(req, (err, is_valid) => {
+    if (!is_valid || !provider.outcome_service) console.log(false);
   });
+  providers[req.body.lis_person_name_full] = provider;
+  
+  res.writeHead(301,
+    { Location: 'https://cahlr.github.io/OpenITS' + '/?lis_person_name_full=' + req.body.lis_person_name_full }
+  );
+  res.end();
+  //res.send(req.body);
+});
 
 
-app.listen(port, function() {}); //starts the server, alternatively you can use app.listen(port)
+app.get('/grade', function (req, res) {
+  console.log("Grade get");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send("Server only accepts post requests.");
+});
+
+app.post('/grade', function (req, res) {
+  /*
+  Takes in a body with 2 params:
+  :param lis_person_name_full: Name, must match from the LTI request
+  :param score: Float from range [0, 1]
+  */
+  console.log("Grade post");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  console.log(req.body);
+  provider = providers[req.body.lis_person_name_full];
+  provider.outcome_service.send_replace_result(parseFloat(req.body.score), (err, result) => {
+    if (!result) {
+      console.log(result);
+    }
+  });
+  res.send("Graded");
+});
+
+
+app.listen(port, function () { });
