775c65c4a561aafae31ff9b7b42c4c4561dcadd6
matthew29tang <matthew29tang@gmail.com>
2021-03-01 12:32:45 -0800

    v1.04 partial changes

--- src/ProblemPool/domain26/steps/domain26a/tutoring/domain26aDefaultPathway.js
+++ src/ProblemPool/domain26/steps/domain26a/tutoring/domain26aDefaultPathway.js
@@ -1,1 +1,1 @@
-var hints = [{id: "domain26a-h2", type: "hint", dependencies: [], title: "Answer", text: "Since cube roots are defined for positive, negative, and zero numbers, there are no values of x that would make the function undefined. So, the domain is $$(-\\infty,\\infty).$$,"}, ]; export {hints};
+var hints = [{id: "domain26a-h2", type: "hint", dependencies: [], title: "Answer", text: "Since cube roots are defined for positive, negative, and zero numbers, there are no values of x that would make the function undefined. So, the domain is $$(-\\infty,\\infty).$$"}, ]; export {hints};
