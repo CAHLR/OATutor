9f373d578a0635909abd9aca0bf57c3bc00c6b47
matthew29tang <matthew29tang@gmail.com>
2021-03-01 12:32:45 -0800

    v1.04 partial changes

--- src/ProblemPool/SubAdd16/steps/SubAdd16a/SubAdd16a.js
+++ src/ProblemPool/SubAdd16/steps/SubAdd16a/SubAdd16a.js
@@ -1,1 +1,1 @@
-import hints from "./SubAdd16a-index.js"; const step = {id: "SubAdd16a", stepAnswer: ["c=-3.67"], problemType: "TextBox", stepTitle: "c-0.93=-4.6", stepBody: "", answerType: "arithmetic", hints: hints}; export {step};
+import React from 'react'; import { InlineMath } from 'react-katex';import hints from "./SubAdd16a-index.js"; const step = {id: "SubAdd16a", stepAnswer: ["c=-3.67"], problemType: "TextBox", stepTitle: <div> <InlineMath math="c-\left(0.93\right)=-\left(4.6\right)"/></div>, stepBody: "", answerType: "arithmetic", hints: hints}; export {step};
