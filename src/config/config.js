1f86f808a09b2ef1bc85326ae082246e2723f4de
Ethan Zhang <36081769+e-zhang09@users.noreply.github.com>
2021-11-22 14:44:24 -0800

    V1.2.4: LTI lesson selection fixes, MC ordering fixes, Firebase Content field

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ import React from 'react';
 import courses from './coursePlans.js';
 
 const ThemeContext = React.createContext(0);
-const siteVersion = "1.2.3";
+const siteVersion = "1.2.4";
 const logData = true;
 const logMouseData = false;
 const debug = false;
