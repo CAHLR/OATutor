004e6f0a8c1a3aadabe7de7b3480f4177a3981a9
Ethan Zhang <36081769+e-zhang09@users.noreply.github.com>
2021-11-29 02:11:34 -0800

    feat: add max restart to pm2 config [skip ci]

--- new-lti-server/ecosystem.config.js
+++ new-lti-server/ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
     script: 'app.js',
     watch: '.',
     watch_delay: 1000,
+
+    max_restarts: 10,
+    restart_delay: 500,
+    autorestart: true,
+
     env: {
       PORT: 3003
     }
