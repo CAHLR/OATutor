1b8b65d20865d4c71f2841d9c7fe981e1f5992ee
Ethan Zhang <36081769+e-zhang09@users.noreply.github.com>
2022-03-26 17:01:53 -0700

    chore: content update, Sat Mar 26 17:01:53 PDT 2022
    content script commit: CAHLR/OpenITS-content@e531abd874b1e1484f0fb56112701e51b5294faa

---
+++ src/ProblemPool/aa97d5brate2/steps/aa97d5brate2a/aa97d5brate2a.js
@@ -0,0 +1,1 @@
+import hints from "./aa97d5brate2a-index.js"; const step = {id: "aa97d5brate2a", stepAnswer: ["4"], problemType: "TextBox", stepTitle: "$$p(x)=4x+2$$ on $$[33+h]$$", stepBody: "", answerType: "arithmetic", hints: hints, variabilization: {}}; export {step};
