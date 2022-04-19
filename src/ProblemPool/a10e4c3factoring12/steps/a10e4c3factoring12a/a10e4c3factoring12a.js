a71fbfce76715aa2e0213461d681f3f96381f297
Ethan Zhang <36081769+e-zhang09@users.noreply.github.com>
2022-04-18 22:34:05 -0700

    V1.3.4: change lesson ID scheme

---
+++ src/ProblemPool/a10e4c3factoring12/steps/a10e4c3factoring12a/a10e4c3factoring12a.js
@@ -0,0 +1,1 @@
+import hints from "./a10e4c3factoring12a-index.js"; const step = {id: "a10e4c3factoring12a", stepAnswer: ["$$5x^{2\\left(x-3\\right)} \\left(x+5\\right)$$"], problemType: "TextBox", stepTitle: "$$5x^4+10x^3-75x^2$$", stepBody: "", answerType: "arithmetic", hints: hints, variabilization: {}}; export {step};
