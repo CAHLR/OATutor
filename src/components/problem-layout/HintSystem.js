dd02b3dc68352ab0c112464a31c4d1be8507defa
Xiaowen Yu <yxw2002@berkeley.edu>
2023-06-23 22:20:14 +0800

    Initial commit to new repo

--- src/components/problem-layout/HintSystem.js
+++ src/components/problem-layout/HintSystem.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import HintTextbox from './HintTextbox.js';
-import { renderText, chooseVariables } from '../../platform-logic/renderText.js';
-import SubHintSystem from './SubHintSystem.js';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import HintTextbox from "./HintTextbox.js";
+import {
+    renderText,
+    chooseVariables,
+} from "../../platform-logic/renderText.js";
+import SubHintSystem from "./SubHintSystem.js";
 import { ThemeContext } from "../../config/config";
 import Spacer from "../Spacer";
 import { stagingProp } from "../../util/addStagingProperty";
@@ -18,20 +21,28 @@ class HintSystem extends React.Component {
 
     constructor(props) {
         super(props);
+
         var subHintsFinished = [];
         for (var i = 0; i < this.props.hints.length; i++) {
-            subHintsFinished.push(new Array((this.props.hints[i].subHints !== undefined ? this.props.hints[i].subHints.length : 0)).fill(0));
+            subHintsFinished.push(
+                new Array(
+                    this.props.hints[i].subHints !== undefined
+                        ? this.props.hints[i].subHints.length
+                        : 0
+                ).fill(0)
+            );
         }
 
         this.giveStuFeedback = props.giveStuFeedback;
         this.unlockFirstHint = props.unlockFirstHint;
+
         this.state = {
             latestStep: 0,
             currentExpanded: this.unlockFirstHint ? 0 : -1,
             hintAnswer: "",
             showSubHints: new Array(this.props.hints.length).fill(false),
-            subHintsFinished: subHintsFinished
-        }
+            subHintsFinished: subHintsFinished,
+        };
 
         if (this.unlockFirstHint && this.props.hintStatus.length > 0) {
             this.props.unlockHint(0, this.props.hints[0].type);
@@ -48,54 +59,66 @@ class HintSystem extends React.Component {
             }
             this.setState({ latestStep: i });
         }
-    }
+    };
 
     isLocked = (hintNum) => {
         if (hintNum === 0) {
             return false;
         }
         var dependencies = this.props.hints[hintNum].dependencies;
-        var isSatisfied = dependencies.every(dependency => this.props.hintStatus[dependency] === 1);
+        var isSatisfied = dependencies.every(
+            (dependency) => this.props.hintStatus[dependency] === 1
+        );
         return !isSatisfied;
-    }
+    };
 
     toggleSubHints = (event, i) => {
-        this.setState(prevState => {
-            var displayHints = prevState.showSubHints;
-            displayHints[i] = !displayHints[i];
-            return ({
-                showSubHints: displayHints
-            })
-        }, () => {
-            this.props.answerMade(this.index, this?.step?.knowledgeComponents, false);
-        });
-    }
+        this.setState(
+            (prevState) => {
+                var displayHints = prevState.showSubHints;
+                displayHints[i] = !displayHints[i];
+                return {
+                    showSubHints: displayHints,
+                };
+            },
+            () => {
+                this.props.answerMade(
+                    this.index,
+                    this?.step?.knowledgeComponents,
+                    false
+                );
+            }
+        );
+    };
 
     unlockSubHint = (hintNum, i, isScaffold) => {
-        this.setState(prevState => {
-            prevState.subHintsFinished[i][hintNum] = (!isScaffold ? 1 : 0.5);
-            return { subHintsFinished: prevState.subHintsFinished }
-        }, () => {
-            this.context.firebase.log(
-                null,
-                this.props.problemID,
-                this.step,
-                null,
-                null,
-                this.state.subHintsFinished,
-                "unlockSubHint",
-                chooseVariables(this.props.stepVars, this.props.seed),
-                this.props.lesson,
-                this.props.courseName
-            );
-        });
-    }
+        this.setState(
+            (prevState) => {
+                prevState.subHintsFinished[i][hintNum] = !isScaffold ? 1 : 0.5;
+                return { subHintsFinished: prevState.subHintsFinished };
+            },
+            () => {
+                this.context.firebase.log(
+                    null,
+                    this.props.problemID,
+                    this.step,
+                    null,
+                    null,
+                    this.state.subHintsFinished,
+                    "unlockSubHint",
+                    chooseVariables(this.props.stepVars, this.props.seed),
+                    this.props.lesson,
+                    this.props.courseName
+                );
+            }
+        );
+    };
 
     submitSubHint = (parsed, hint, isCorrect, i, hintNum) => {
         if (isCorrect) {
-            this.setState(prevState => {
+            this.setState((prevState) => {
                 prevState.subHintsFinished[i][hintNum] = 1;
-                return { subHintsFinished: prevState.subHintsFinished }
+                return { subHintsFinished: prevState.subHintsFinished };
             });
         }
         this.context.firebase.hintLog(
@@ -105,11 +128,14 @@ class HintSystem extends React.Component {
             hint,
             isCorrect,
             this.state.hintsFinished,
-            chooseVariables(Object.assign({}, this.props.stepVars, hint.variabilization), this.props.seed),
+            chooseVariables(
+                Object.assign({}, this.props.stepVars, hint.variabilization),
+                this.props.seed
+            ),
             this.props.lesson,
             this.props.courseName
         );
-    }
+    };
 
     render() {
         const { classes, index, hints, problemID, seed, stepVars } = this.props;
@@ -118,78 +144,148 @@ class HintSystem extends React.Component {
 
         return (
             <div className={classes.root}>
-                {hints.map((hint, i) =>
-                    <Accordion key={`${problemID}-${hint.id}`}
-                        onChange={(event, expanded) => this.unlockHint(event, expanded, i)}
-                        disabled={this.isLocked(i) && !(use_expanded_view && debug)}
-                        expanded={currentExpanded === i || (use_expanded_view != null && use_expanded_view && debug)}
-                        defaultExpanded={false}>
+                {this.giveDynamicHint && <div>hi</div>}
+                {hints.map((hint, i) => (
+                    <Accordion
+                        key={`${problemID}-${hint.id}`}
+                        onChange={(event, expanded) =>
+                            this.unlockHint(event, expanded, i)
+                        }
+                        disabled={
+                            this.isLocked(i) && !(use_expanded_view && debug)
+                        }
+                        expanded={
+                            currentExpanded === i ||
+                            (use_expanded_view != null &&
+                                use_expanded_view &&
+                                debug)
+                        }
+                        defaultExpanded={false}
+                    >
                         <AccordionSummary
-                            expandIcon={<ExpandMoreIcon/>}
+                            expandIcon={<ExpandMoreIcon />}
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                             {...stagingProp({
-                                "data-selenium-target": `hint-expand-${i}-${index}`
+                                "data-selenium-target": `hint-expand-${i}-${index}`,
                             })}
                         >
                             <Typography className={classes.heading}>
-                                Hint {i + 1}: {renderText((hint.title === "nan" ? "" : hint.title), problemID, chooseVariables(Object.assign({}, stepVars, hint.variabilization), seed), this.context)}</Typography>
+                                Hint {i + 1}:{" "}
+                                {renderText(
+                                    hint.title === "nan" ? "" : hint.title,
+                                    problemID,
+                                    chooseVariables(
+                                        Object.assign(
+                                            {},
+                                            stepVars,
+                                            hint.variabilization
+                                        ),
+                                        seed
+                                    ),
+                                    this.context
+                                )}
+                            </Typography>
                         </AccordionSummary>
                         <AccordionDetails>
-                            <Typography component={'span'} style={{ width: "100%" }}>
-                                {renderText(hint.text, problemID, chooseVariables(Object.assign({}, stepVars, hint.variabilization), seed), this.context)}
-                                {hint.type === "scaffold" ?
+                            <Typography
+                                component={"span"}
+                                style={{ width: "100%" }}
+                            >
+                                {renderText(
+                                    hint.text,
+                                    problemID,
+                                    chooseVariables(
+                                        Object.assign(
+                                            {},
+                                            stepVars,
+                                            hint.variabilization
+                                        ),
+                                        seed
+                                    ),
+                                    this.context
+                                )}
+                                {hint.type === "scaffold" ? (
                                     <div>
-                                        <Spacer/>
-                                        <HintTextbox hintNum={i} hint={hint}
-                                                     index={index}
-                                                     submitHint={this.props.submitHint}
-                                                     seed={this.props.seed}
-                                                     hintVars={Object.assign({}, this.props.stepVars, hint.variabilization)}
-                                                     toggleHints={(event) => this.toggleSubHints(event, i)}
-                                                     giveStuFeedback={this.giveStuFeedback}
+                                        <Spacer />
+                                        <HintTextbox
+                                            hintNum={i}
+                                            hint={hint}
+                                            index={index}
+                                            submitHint={this.props.submitHint}
+                                            seed={this.props.seed}
+                                            hintVars={Object.assign(
+                                                {},
+                                                this.props.stepVars,
+                                                hint.variabilization
+                                            )}
+                                            toggleHints={(event) =>
+                                                this.toggleSubHints(event, i)
+                                            }
+                                            giveStuFeedback={
+                                                this.giveStuFeedback
+                                            }
                                         />
-                                    </div> : ""}
-                                {(showSubHints[i] || (use_expanded_view && debug)) && hint.subHints !== undefined ?
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                                {(showSubHints[i] ||
+                                    (use_expanded_view && debug)) &&
+                                hint.subHints !== undefined ? (
                                     <div className="SubHints">
-                                        <Spacer/>
-                                        <ErrorBoundary componentName={"SubHintSystem"}>
+                                        <Spacer />
+                                        <ErrorBoundary
+                                            componentName={"SubHintSystem"}
+                                        >
                                             <SubHintSystem
-                                                giveStuFeedback={this.giveStuFeedback}
-                                                unlockFirstHint={this.unlockFirstHint}
+                                                giveStuFeedback={
+                                                    this.giveStuFeedback
+                                                }
+                                                unlockFirstHint={
+                                                    this.unlockFirstHint
+                                                }
                                                 problemID={problemID}
                                                 hints={hint.subHints}
                                                 unlockHint={this.unlockSubHint}
-                                                hintStatus={this.state.subHintsFinished[i]}
+                                                hintStatus={
+                                                    this.state.subHintsFinished[
+                                                        i
+                                                    ]
+                                                }
                                                 submitHint={this.submitSubHint}
                                                 parent={i}
                                                 index={index}
                                                 seed={this.props.seed}
-                                                hintVars={Object.assign({}, this.props.stepVars, hint.variabilization)}
+                                                hintVars={Object.assign(
+                                                    {},
+                                                    this.props.stepVars,
+                                                    hint.variabilization
+                                                )}
                                             />
                                         </ErrorBoundary>
-                                        <Spacer/>
+                                        <Spacer />
                                     </div>
-                                    : ""}
+                                ) : (
+                                    ""
+                                )}
                             </Typography>
                         </AccordionDetails>
                     </Accordion>
-                )}
+                ))}
             </div>
-        )
-    };
-
+        );
+    }
 }
 
-const styles = theme => ({
+const styles = (theme) => ({
     root: {
-        width: '100%',
+        width: "100%",
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
         fontWeight: theme.typography.fontWeightRegular,
     },
 });
-
 
 export default withStyles(styles)(HintSystem);
--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import courses from '../content-sources/oatutor/coursePlans.json';
+import React from "react";
+import courses from "../content-sources/oatutor/coursePlans.json";
 import { calculateSemester } from "../util/calculateSemester.js";
 
-import { SITE_NAME } from "@common/global-config"
+import { SITE_NAME } from "@common/global-config";
 import { cleanObjectKeys } from "../util/cleanObject";
 
 const ThemeContext = React.createContext(0);
 const SITE_VERSION = "1.5.0";
 
-const CURRENT_SEMESTER = calculateSemester(Date.now())
+const CURRENT_SEMESTER = calculateSemester(Date.now());
 
 /**
  * If user does not access the website through Canvas, show a warning (for the first time).
@@ -57,47 +57,67 @@ const ENABLE_BOTTOM_OUT_HINTS = true;
 
 // DynamicText not supported for HTML body types
 const dynamicText = {
-    "%CAR%": "Tesla car"
+    "%CAR%": "Tesla car",
 };
 
-const _SHORT_SITE_NAME = SITE_NAME.toLowerCase().replace(/[^a-z]/g, '').substr(0, 16)
+const _SHORT_SITE_NAME = SITE_NAME.toLowerCase()
+    .replace(/[^a-z]/g, "")
+    .substr(0, 16);
 
-const USER_ID_STORAGE_KEY = `${_SHORT_SITE_NAME}-user_id`
-const PROGRESS_STORAGE_KEY = `${_SHORT_SITE_NAME}-progress`
-export const LESSON_PROGRESS_STORAGE_KEY = lessonId => `${PROGRESS_STORAGE_KEY}-${lessonId}`
+const USER_ID_STORAGE_KEY = `${_SHORT_SITE_NAME}-user_id`;
+const PROGRESS_STORAGE_KEY = `${_SHORT_SITE_NAME}-progress`;
+export const LESSON_PROGRESS_STORAGE_KEY = (lessonId) =>
+    `${PROGRESS_STORAGE_KEY}-${lessonId}`;
 
-const CANVAS_WARNING_STORAGE_KEY = `${_SHORT_SITE_NAME}-canvas-warning-dismissed`
+const CANVAS_WARNING_STORAGE_KEY = `${_SHORT_SITE_NAME}-canvas-warning-dismissed`;
 
 // Firebase Config
 const MAX_BUFFER_SIZE = 100;
 const GRANULARITY = 5;
 
-const EQUATION_EDITOR_AUTO_COMMANDS = "pi theta sqrt sum prod int alpha beta gamma rho nthroot pm";
+const EQUATION_EDITOR_AUTO_COMMANDS =
+    "pi theta sqrt sum prod int alpha beta gamma rho nthroot pm";
 const EQUATION_EDITOR_AUTO_OPERATORS = "sin cos tan";
 
-const MIDDLEWARE_URL = process.env.REACT_APP_MIDDLEWARE_URL || "https://oatutor.askoski.berkeley.edu";
+const MIDDLEWARE_URL =
+    process.env.REACT_APP_MIDDLEWARE_URL ||
+    "https://oatutor.askoski.berkeley.edu";
 
-const HELP_DOCUMENT = "https://docs.google.com/document/d/e/2PACX-1vToe2F3RiCx1nwcX9PEkMiBA2bFy9lQRaeWIbyqlc8W_KJ9q-hAMv34QaO_AdEelVY7zjFAF1uOP4pG/pub"
+const HELP_DOCUMENT =
+    "https://docs.google.com/document/d/e/2PACX-1vToe2F3RiCx1nwcX9PEkMiBA2bFy9lQRaeWIbyqlc8W_KJ9q-hAMv34QaO_AdEelVY7zjFAF1uOP4pG/pub";
+
+const DYNAMIC_HINT_URL = "https://oatutor-backend.herokuapp.com/get_hint";
+
+const DYNAMIC_HINT_TEMPLATE =
+    "<{problem_title}.> <{problem_subtitle}.> <{question_title}.> <{question_subtitle}.> <Student's answer is: {student_answer}.> <The correct answer is: {correct_answer}.> Please give a hint for this.";
 
 // const coursePlans = courses.sort((a, b) => a.courseName.localeCompare(b.courseName));
 const coursePlans = courses;
-const _coursePlansNoEditor = coursePlans.filter(({ editor }) => !!!editor)
+const _coursePlansNoEditor = coursePlans.filter(({ editor }) => !!!editor);
 
 const lessonPlans = [];
 for (let i = 0; i < coursePlans.length; i++) {
     const course = coursePlans[i];
     for (let j = 0; j < course.lessons.length; j++) {
-        course.lessons[j].learningObjectives = cleanObjectKeys(course.lessons[j].learningObjectives)
-        lessonPlans.push({ ...course.lessons[j], courseName: course.courseName,
-                                                courseOER: course.courseOER != null ? course.courseOER : "",
-                                                courseLicense: course.courseLicense != null ? course.courseLicense : ""});
+        course.lessons[j].learningObjectives = cleanObjectKeys(
+            course.lessons[j].learningObjectives
+        );
+        lessonPlans.push({
+            ...course.lessons[j],
+            courseName: course.courseName,
+            courseOER: course.courseOER != null ? course.courseOER : "",
+            courseLicense:
+                course.courseLicense != null ? course.courseLicense : "",
+        });
     }
 }
-const _lessonPlansNoEditor = lessonPlans.filter(({ courseName }) => !courseName.startsWith("!!"))
+const _lessonPlansNoEditor = lessonPlans.filter(
+    ({ courseName }) => !courseName.startsWith("!!")
+);
 
 const findLessonById = (ID) => {
-    return _lessonPlansNoEditor.find(lessonPlan => lessonPlan.id === ID)
-}
+    return _lessonPlansNoEditor.find((lessonPlan) => lessonPlan.id === ID);
+};
 
 export {
     ThemeContext,
@@ -116,6 +136,8 @@ export {
     EQUATION_EDITOR_AUTO_COMMANDS,
     EQUATION_EDITOR_AUTO_OPERATORS,
     MIDDLEWARE_URL,
+    DYNAMIC_HINT_URL,
+    DYNAMIC_HINT_TEMPLATE,
     USER_ID_STORAGE_KEY,
     PROGRESS_STORAGE_KEY,
     SITE_NAME,
