5fe690d1b9b7e0d457873baf4bce92647a81795b
SagaLGIT <saga.lundborg@gmail.com>
2024-07-09 01:54:11 +0200

    Math to speech in progress
    Speakable hints generated but not correct alignment

---
+++ src/a-math-to-speech/hintScraper.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+# Path to the source file
+#source_file = 'src/content-sources/oatutor/content-pool/a0a04b1divmonomial1/steps/a0a04b1divmonomial1a/tutoring/a0a04b1divmonomial1aDefaultPathway.json'
+# Retrieve filepath of all files with DefaultPathway in content-pool directory
+output_file = 'src/a-math-to-speech/hintStrings.txt'
+
+filepaths = []
+for root, dirs, files in os.walk('src/content-sources/oatutor/content-pool'):
+    for file in files:
+        if 'DefaultPathway' in file:
+            filepaths.append(os.path.join(root, file))
+
+with open(output_file, 'w') as outfile:
+    for source_file in filepaths:
+        # Read the contents of the source file
+
+        with open(source_file, 'r') as file:
+            #contents = file.read()
+            data = json.load(file)
+
+            # Extract the "text" attribute from each JSON object
+            contents = [obj['text'] for obj in data]
+
+            # Write the contents to the output file
+            for content in contents:
+                outfile.write(content + "\n")
+
+print(f"Contents of {source_file} have been written to {output_file}.")
