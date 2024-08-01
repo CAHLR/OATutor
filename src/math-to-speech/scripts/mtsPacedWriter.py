541b09be6f4a68aa57b22baa05e0f015de67f41f
SagaLGIT <saga.lundborg@gmail.com>
2024-07-31 19:43:47 -0700

    pacedSpeech attribute added

--- src/math-to-speech/scripts/mtsPacedWriter.py
+++ src/math-to-speech/scripts/mtsPacedWriter.py
@@ -12,27 +12,24 @@ for root, dirs, files in os.walk('src/content-sources/oatutor/content-pool'):  #
 
 
 # Read the contents of the combinedFinishedHints.txt file
-with open('src/math-to-speech/hint-text-files/conversion-math/translatedHintsPost.txt', 'r', encoding='utf-8') as file:  # TODO: Change this path
+# with open('src/math-to-speech/hint-text-files/conversion-math/translatedHintsPost.txt', 'r', encoding='utf-8') as file:  # TODO: Change this path
+#     hints = file.readlines()
+#     hints = [hint.strip() for hint in hints]
+
+with open('src/math-to-speech/finished-translations/pacedTranslatedHintsPost.txt', 'r', encoding='utf-8') as file:  
     hints = file.readlines()
-    hints = [hint.strip() for hint in hints]
+    hints = [hint.strip().split('£') for hint in hints]
 
-# with open('src/math-to-speech/finished-translations/math.txt', 'r', encoding='utf-8') as file:  # corrected
-#     maths = file.readlines()
-#     maths = [  math.strip().split('@') for math in maths]
-
-i = 0
+i = 0 
 for source_file in filepaths:
     # Read the contents of the source file
 
     with open(source_file, 'r+', encoding='utf-8') as file:
         data = json.load(file)
 
-        # Update the "text" attribute with hints[i]
         for obj in data:
             obj['pacedSpeech'] = hints[i]
-            # if maths[i] != '':
-            #     obj['math'] = maths[i]
-            # i+=1
+            i+=1
        
         # Write the updated JSON object to the source file
         file.seek(0)
