5503bc66227f993583396f5ca97e16ac740adc68
Xiaowen Yu <yxw2002@berkeley.edu>
2024-09-15 16:29:08 -0700

    enable keystroke logging in oatutor main repo

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,22 +1,12 @@
-// const config = {
-//     apiKey: "[apikey]",
-//     authDomain: "[projId].firebaseapp.com",
-//     databaseURL: "https://[projId].firebaseio.com",
-//     projectId: "[projId]",
-//     storageBucket: "[projId].appspot.com",
-//     messagingSenderId: "[messagingSenderId]",
-//     appId: "[appId]",
-//     measurementId: "[measurementId]",
-// };
-
 const config = {
-    apiKey: "AIzaSyDKr8r7o0vivu85fyRhn7bIn6j5GxljX8w",
-    authDomain: "oatutor-askoski.firebaseapp.com",
-    projectId: "oatutor-askoski",
-    storageBucket: "oatutor-askoski.appspot.com",
-    messagingSenderId: "1050614582892",
-    appId: "1:1050614582892:web:f7ffec360148dc17da1249",
-    measurementId: "G-44V1SE1GEF",
+    apiKey: "[apikey]",
+    authDomain: "[projId].firebaseapp.com",
+    databaseURL: "https://[projId].firebaseio.com",
+    projectId: "[projId]",
+    storageBucket: "[projId].appspot.com",
+    messagingSenderId: "[messagingSenderId]",
+    appId: "[appId]",
+    measurementId: "[measurementId]",
 };
 
 export default config;
