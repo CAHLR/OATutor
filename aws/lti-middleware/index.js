39cb206dfb90891fa3bba52f073124a856b6d00d
Anirudh Pai <anirudhnpai@gmail.com>
2024-10-07 12:28:45 -0700

    back to ssl

--- aws/lti-middleware/index.js
+++ aws/lti-middleware/index.js
@@ -29,7 +29,7 @@ const consumerKeySecretMap = {
   key: "secret",
 };
 
-const oatsHost = "http://quadro.ist.berkeley.edu:2030/#";
+const oatsHost = "https://quadro.ist.berkeley.edu:2030/#";
 const unlinkedPage = "assignment-not-linked";
 const alreadyLinkedPage = "assignment-already-linked";
 const jwtAlgorithm = "HS256";
