36f91656cbaa69c964af2c1a0bf0fe74f6eab6c8
allyqwert <107647820+allyqwert@users.noreply.github.com>
2025-10-23 02:41:19 -0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,12 +1,13 @@
-const config = {
-    apiKey: "[apikey]",
-    authDomain: "[projId].firebaseapp.com",
-    databaseURL: "https://[projId].firebaseio.com",
-    projectId: "[projId]",
-    storageBucket: "[projId].appspot.com",
-    messagingSenderId: "[messagingSenderId]",
-    appId: "[appId]",
-    measurementId: "[measurementId]",
-};
+// const config = {
+//     apiKey: "[apikey]",
+//     authDomain: "[projId].firebaseapp.com",
+//     databaseURL: "https://[projId].firebaseio.com",
+//     projectId: "[projId]",
+//     storageBucket: "[projId].appspot.com",
+//     messagingSenderId: "[messagingSenderId]",
+//     appId: "[appId]",
+//     measurementId: "[measurementId]",
+// };
 
+const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 export default config;
