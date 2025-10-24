e6215a7349eb1ee503a96e4b7d9c8b0af969d86b
allyqwert <107647820+allyqwert@users.noreply.github.com>
2025-10-23 20:55:47 -0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -10,23 +10,24 @@
 // };
 
 
-// config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-// export default firebase;
-
-
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
-
-let config = {};
-try {
-  config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-} catch (e) {
-  console.error("Firebase config parse failed. Make sure REACT_APP_FIREBASE_CONFIG is set correctly.", e);
-}
-
-if (!firebase.apps.length && Object.keys(config).length) {
-  firebase.initializeApp(config);
-}
+config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 export default firebase;
+
+
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/firestore";
+// import "firebase/compat/auth";
+
+// let config = {};
+// try {
+//   config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+// } catch (e) {
+//   console.error("Firebase config parse failed. Make sure REACT_APP_FIREBASE_CONFIG is set correctly.", e);
+// }
+
+// if (!firebase.apps.length && Object.keys(config).length) {
+//   firebase.initializeApp(config);
+// }
+
+// export default firebase;
