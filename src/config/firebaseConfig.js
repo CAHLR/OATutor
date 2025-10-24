c5d8b81d8aa3cf121bd8dbf5db3f7140d345f24c
allyqwert <107647820+allyqwert@users.noreply.github.com>
2025-10-23 22:38:18 -0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -10,9 +10,8 @@
 // };
 
 
-const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
-
-export default config;
+// const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+// export default config;
 
 
 // import firebase from "firebase/compat/app";
@@ -31,3 +30,20 @@ export default config;
 // }
 
 // export default firebase;
+
+import { initializeApp, getApps } from "firebase/app";
+
+let config = {};
+try {
+  config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG || "{}");
+} catch (err) {
+  console.error("Invalid Firebase config:", err);
+}
+
+let app;
+if (!getApps().length) {
+  app = initializeApp(config);
+}
+
+export default config;
+export { app };
